5279eaff2cd312c0ab6ac1a61225ae2e97003b6e
Tim McIver <tim@timmciver.com>
2016-03-31 22:46:52 -0400

    Some investigations of code in section titled 'Sum and Product'.

--- src/Chapter11.hs
+++ src/Chapter11.hs
@@ -3,6 +3,18 @@
 
 module Chapter11 ( Goats(..)
                  , tooMany
+                 , Product(..)
+                 , Sum(..)
+                 , RecordProduct
+                 , Name
+                 , Age
+                 , LovesMud
+                 , PoundsOfWool
+                 , CowInfo(..)
+                 , PigInfo(..)
+                 , SheepInfo(..)
+                 , Animal(..)
+                 , Animal'
                  ) where
 
 class TooMany a where
@@ -30,3 +42,35 @@ instance TooMany (Int, Int) where
 -- section 11.7, exercise 3
 instance (Num a, TooMany a) => TooMany (a, a) where
   tooMany (x, y) = tooMany (x + y)
+
+data Product a b = Product a b
+                 deriving (Eq, Show)
+
+data Sum a b = First a
+             | Second b
+             deriving (Eq, Show)
+
+data RecordProduct a b = RecordProduct { pfirst :: a
+                                       , psecond :: b }
+                       deriving (Eq, Show)
+
+type Name = String
+type Age = Int
+type LovesMud = Bool
+type PoundsOfWool = Int
+
+data CowInfo = CowInfo Name Age
+             deriving (Eq, Show)
+
+data PigInfo = PigInfo Name Age LovesMud
+             deriving (Eq, Show)
+
+data SheepInfo = SheepInfo Name Age PoundsOfWool
+               deriving (Eq, Show)
+
+data Animal = Cow CowInfo
+            | Pig PigInfo
+            | Sheep SheepInfo
+            deriving (Eq, Show)
+
+type Animal' = Sum CowInfo (Sum PigInfo SheepInfo)
