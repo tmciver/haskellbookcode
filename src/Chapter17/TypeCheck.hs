c2caa59cb2569fc9dfc0cdee61fd0d0d638dbc7c
Tim McIver <tim@timmciver.com>
2016-04-13 23:04:54 -0400

    Some exercises from chapter 17.

---
+++ src/Chapter17/TypeCheck.hs
@@ -0,0 +1,26 @@
+import Control.Applicative
+import Data.List (elemIndex)
+
+added :: Maybe Integer
+added = (+3) <$> (lookup 3 $ zip [1, 2, 3] [4, 5, 6])
+
+y :: Maybe Integer
+y = lookup 3 $ zip [1, 2, 3] [4, 5, 6]
+
+z :: Maybe Integer
+z = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
+
+tupled :: Maybe (Integer, Integer)
+tupled = liftA2 (,) y z
+
+m :: Maybe Int
+m = elemIndex 3 [1, 2, 3, 4, 5]
+
+n :: Maybe Int
+n = elemIndex 4 [1, 2, 3, 4, 5]
+
+max' :: Int -> Int -> Int
+max' = max
+
+maxed :: Maybe Int
+maxed = max' <$> m <*> n
