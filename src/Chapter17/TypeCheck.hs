472c05c6f03a58777fc60ecd21b4cdf6ab3cfd4d
Tim McIver <tim@timmciver.com>
2016-04-17 21:30:52 -0400

    Added implementation for `Identity` exercise.

--- src/Chapter17/TypeCheck.hs
+++ src/Chapter17/TypeCheck.hs
@@ -24,3 +24,13 @@ max' = max
 
 maxed :: Maybe Int
 maxed = max' <$> m <*> n
+
+newtype Identity a = Identity a
+                   deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity x) = Identity (f x)
+
+instance Applicative Identity where
+  pure = Identity
+  Identity f <*> Identity x = Identity (f x)
