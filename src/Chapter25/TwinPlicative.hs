25278112bf30c04829c3d9e2999c2b9848e5e628
Tim McIver <tim@timmciver.com>
2016-05-03 22:40:42 -0400

    Implement `pure` for Compose. <*> seems a bit trickier.

--- src/Chapter25/TwinPlicative.hs
+++ src/Chapter25/TwinPlicative.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE InstanceSigs #-}
+-- {-# LANGUAGE InstanceSigs #-}
 
 module Chapter25.TwinPlicative where
 
@@ -9,10 +9,11 @@ instance (Functor f, Functor g) => Functor (Compose f g) where
   fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
 
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
-  pure :: a -> Compose f g a
-  pure = undefined
+--  pure :: a -> Compose [] [] a
+  pure = Compose . pure . pure
 
-  (<*>) :: Compose f g (a -> b)
-        -> Compose f g a
-        -> Compose f g b
+  -- (<*>) :: Compose f g (a -> b)
+  --       -> Compose f g a
+  --       -> Compose f g b
   (Compose f) <*> (Compose a) = undefined
+--  (Compose f) <*> (Compose a) = Compose (f <*> a)
