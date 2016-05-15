28bd411dc54f8339e967585acff47c1bf937a350
Tim McIver <tim@timmciver.com>
2016-05-14 23:40:41 -0400

    Finally solved `TwinPlicative`.

--- src/Chapter25/TwinPlicative.hs
+++ src/Chapter25/TwinPlicative.hs
@@ -1,4 +1,4 @@
--- {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Chapter25.TwinPlicative where
 
@@ -9,11 +9,15 @@ instance (Functor f, Functor g) => Functor (Compose f g) where
   fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
 
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
---  pure :: a -> Compose [] [] a
+  pure :: a -> Compose f g a
   pure = Compose . pure . pure
 
-  -- (<*>) :: Compose f g (a -> b)
-  --       -> Compose f g a
-  --       -> Compose f g b
-  (Compose f) <*> (Compose a) = undefined
---  (Compose f) <*> (Compose a) = Compose (f <*> a)
+  (<*>) :: Compose f g (a -> b)
+        -> Compose f g a
+        -> Compose f g b
+--  (Compose f) <*> (Compose a) = Compose (fmap (\gab -> fmap (\ga -> gab <*> ga) a) f)
+--  (Compose f) <*> (Compose a) = Compose ((fmap . fmap) (\g -> (fmap . fmap) g a) f)
+-- came to the following working solution after some helpful hints from someone
+-- (can't rememer who) on IRC.  Good thing too; I never would have gotten this
+-- on my own. :(
+  (Compose f) <*> (Compose a) = Compose $ fmap (<*>) f <*> a
