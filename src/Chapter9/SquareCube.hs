f2fbb42342f13cd619b336e8a71309765bd045f5
Tim McIver <tim@timmciver.com>
2021-03-11 15:08:57 -0500

    Chapter 9, exercise Square Cube, part 2.

--- src/Chapter9/SquareCube.hs
+++ src/Chapter9/SquareCube.hs
@@ -5,4 +5,4 @@ mySqr = [x^2 | x <- [1..5]]
 myCube = [y^3 | y <- [1..5]]
 
 myExp :: [(Integer, Integer)]
-myExp = [(x, y) | x <- mySqr, y <- myCube]
+myExp = [(x, y) | x <- mySqr, y <- myCube, x < 50 && y < 50]
