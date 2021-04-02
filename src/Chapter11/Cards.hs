00ae9abec65692f5f1aa6a304aeec1558d08c8b9
Tim McIver <tim@timmciver.com>
2021-04-01 21:23:14 -0400

    Added comment about cardinality of Card.

--- src/Chapter11/Cards.hs
+++ src/Chapter11/Cards.hs
@@ -23,6 +23,11 @@ data Value =
   | Ace
   deriving (Eq, Show, Enum)
 
+-- What's the cardinality of Card?
+--
+-- cardinality of suit * cardinality of Value + 1 (Joker1) + 1 (Joker2)
+-- 4 * 13 + 2
+-- 54
 data Card = Card Suit Value
           | Joker1
           | Joker2
