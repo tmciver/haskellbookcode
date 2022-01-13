32345fb1283366929fb9361ea527b3ff654da861
Tim McIver <tim@timmciver.com>
2022-01-12 22:30:59 -0500

    Completed exercise "reading comprehension".

--- src/Chapter22/Reader.hs
+++ src/Chapter22/Reader.hs
@@ -5,5 +5,17 @@ newtype Reader r a = Reader { runReader :: r -> a }
 instance Functor (Reader r) where
   fmap f (Reader ra) = Reader (\r -> f (ra r))
 
+-- exercise 1
 ask :: Reader r r
 ask = Reader id
+
+-- exercise: reading comprehension
+myLiftA2 :: Applicative f
+         => (a -> b -> c)
+         -> f a
+         -> f b
+         -> f c
+myLiftA2 f fa fb = f <$> fa <*> fb
+
+asks :: (r -> a) -> Reader r a
+asks f = Reader $ \r -> f r
