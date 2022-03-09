d72a6f5899aad9cb880b23874036078e3c5c719f
Tim McIver <tim@timmciver.com>
2022-03-08 22:32:13 -0500

    Add fraction parser code.

---
+++ src/Chapter24/FractionParser.hs
@@ -0,0 +1,45 @@
+module Chapter24.FractionParser where
+
+import Text.Trifecta
+import Data.Ratio
+
+badFraction = "1/0"
+alsoBad = "10"
+shouldWork = "1/2"
+shouldAlsoWork = "2/1"
+
+parseFraction :: Parser Rational
+parseFraction = do
+  numerator <- decimal
+  char '/'
+  denominator <- decimal
+  return (numerator % denominator)
+
+main :: IO ()
+main = do
+  let parseFraction' = parseString parseFraction mempty
+  print $ parseFraction' shouldWork
+  print $ parseFraction' shouldAlsoWork
+  print $ parseFraction' alsoBad
+  print $ parseFraction' badFraction
+
+-- demo
+
+virtuousFraction :: Parser Rational
+virtuousFraction = do
+  numerator <- decimal
+  char '/'
+  denominator <- decimal
+  case denominator of
+    0 -> fail "Denominator cannot be zero"
+    _ -> return (numerator % denominator)
+
+testVirtuous :: IO ()
+testVirtuous = do
+  let virtuousFraction' = parseString virtuousFraction mempty
+  print $ virtuousFraction' badFraction
+  print $ virtuousFraction' alsoBad
+  print $ virtuousFraction' shouldWork
+  print $ virtuousFraction' shouldAlsoWork
+
+-- demo
